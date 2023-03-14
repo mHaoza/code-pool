fe4d8dbe8e67fa54881220c36499d95a30f6f238
lbh <me@iice.fun>
2023-03-14 10:33:23 +0800

    添加案例，文档

---
+++ components/form/README.md
@@ -0,0 +1,4 @@
+## Form
+* 基于`Vue3`、`Element Plus`封装的Form表单
+* 修改于coderwhy后台管理系统项目中使用的表单
+* 支持条件判断
---
+++ components/form/example.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="panel-roof">
+    <Form
+      v-bind="formConfig"
+      v-model="formData"
+      @change="handleFormChange"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Form from './src/form.vue'
+
+const formConfig = {
+  labelWidth: '140px',
+  formItems: [
+    {
+      field: 'length',
+      type: 'input-number',
+      label: '长',
+      defaultValue: 2,
+      otherOptions: {
+        min: 0,
+        precision: 3
+      }
+    },
+    {
+      field: 'width',
+      type: 'input-number',
+      label: '宽',
+      defaultValue: 2,
+      otherOptions: {
+        min: 0,
+        precision: 3
+      }
+    },
+    {
+      field: 'type',
+      type: 'select',
+      label: '类型',
+      options: [
+        {label: '类型1', code: '0'},
+        {label: '类型2', code: '1'}
+      ],
+      defaultValue: '0'
+    },
+    {
+      field: 'type2',
+      type: 'input-number',
+      label: '次级类型',
+      defaultValue: 0,
+      otherOptions: {
+        min: 0
+      },
+      show: { field: 'type', rule: (val: string) => val !== '0' }
+    }
+  ]
+}
+const formItems = formConfig.formItems
+const formOriginData: any = {}
+
+for (const item of formItems) {
+  formOriginData[item.field] = item.defaultValue
+}
+const formData = ref(formOriginData)
+
+const handleFormChange = (value: any, field: string) => {
+  console.log(field, value)
+}
+</script>
+
+<style scoped></style>
